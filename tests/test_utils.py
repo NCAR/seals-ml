2ad45184f6633ee90d8155e347bc2f7d199c2364
Thomas Martin <38228140+ThomasMGeo@users.noreply.github.com>
2023-09-20 16:16:25 -0600

    Small test update
    Some errors in the testing of the utility functions. These have been addressed.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,6 @@ import pytest
 import numpy as np
 from sealsml.utils import distance_between_points_3d, azimuth, dip
 
-
 def test_distance_between_points_3d():
     """
     Tests the `distance_between_points_3d` function.
@@ -32,11 +31,10 @@ def test_azimuth():
     assert np.allclose(azimuth_result, 45)  # Use np.allclose for floating-point comparisons
 
     # Test that the function raises an error when the points are not valid.
-    point1 = np.array([1, 0, 0])
-    point2 = np.array([1, 2, 0
-                       4, 5, 4])
+    point3 = np.array([1, 0, 0])
+    point4 = np.array([1, 2, 0], [4, 5, 4])
     with pytest.raises(TypeError):
-        azimuth(point1, point2)int2)
+        azimuth(point3, point4)
 
 def test_dip():
     """Tests the `dip` function."""
@@ -48,8 +46,9 @@ def test_dip():
     assert np.allclose(dip_result, 90)  # Use np.allclose for floating-point comparisons
 
     # Test that the function raises an error when the points are not valid.
-    point1 = np.array([0, 0, 1])
-    point2 = np.array([1, 0, 0, 4])
+    point3 = np.array([0, 0, 1])
+    point4 = np.array([1, 0, 0, 4])
     with pytest.raises(TypeError):
-        dip(point1, point2)2)
+        dip(point3, point4)
 
+# the end
