9dc99fe63bcfbf30bc77b05b915f8415d4e59e3e
charlie-becker <cbecker@ucar.edu>
2023-11-28 13:05:01 -0700

    update seed, timestamps, output

--- scripts/train.py
+++ scripts/train.py
@@ -7,18 +7,27 @@ from sealsml.data import Preprocessor
 from sealsml.evaluate import provide_metrics
 from sklearn.model_selection import train_test_split
 from bridgescaler import save_scaler
+import keras
+import numpy as np
+import xarray as xr
+import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--config", help="Path to config file")
 args = parser.parse_args()
-with open("./config/train_transformer.yaml") as config_file:
+with open(args.config) as config_file:
     config = yaml.safe_load(config_file)
 
+keras.utils.set_random_seed(config["random_seed"])
+np.random.seed(config["random_seed"])
 username = os.environ.get('USER')
 config["out_path"] = config["out_path"].replace("username", username)
 
 files = glob.glob(os.path.join(config["data_path"], "*.nc"))
-training, validation = train_test_split(files, test_size=config["validation_ratio"])
+print(files)
+training, validation = train_test_split(files,
+                                        test_size=config["validation_ratio"],
+                                        random_state=config["random_seed"])
 
 p = Preprocessor(scaler_type=config["scaler_type"], sensor_pad_value=-1, sensor_type_value=-999)
 
@@ -32,20 +41,29 @@ scaled_decoder_val, decoder_mask_val = p.preprocess(decoder_data_val, fit_scaler
 
 model = QuantizedTransformer(**config["model"])
 model.compile(**config["model_compile"])
+
 model.fit(x=(scaled_encoder, scaled_decoder, encoder_mask, decoder_mask),
           y=targets,
           validation_data=((scaled_encoder_val, scaled_decoder_val, encoder_mask_val, decoder_mask_val), targets_val),
           **config["model_fit"])
 
 probabilities = model.predict(x=(scaled_encoder_val, scaled_decoder_val, encoder_mask_val, decoder_mask_val),
-                      batch_size=config["predict_batch_size"]).squeeze()
+                              batch_size=config["predict_batch_size"]).squeeze()
 
 metrics = provide_metrics(targets_val, probabilities)
 
 print(metrics)
+date_str = datetime.datetime.now().strftime("%Y-%m-%d")
 
-if config["save"]:
+if config["save_model"]:
 
     os.makedirs(config["out_path"], exist_ok=True)
-    save_scaler(p.scaler, os.path.join(config["out_path"], "scaler.json"))
-    model.save(os.path.join(config["out_path"], "model.keras"))
+    save_scaler(p.scaler, os.path.join(config["out_path"], f"scaler_{date_str}.json"))
+    model.save(os.path.join(config["out_path"], f"model_{date_str}.keras"))
+
+if config["save_output"]:
+
+    output = xr.Dataset(data_vars=dict(targets=(["sample", "pot_leak_locs"], targets_val),
+                                       probabilities=(["sample", "pot_leak_locs"], probabilities)))
+    output.to_netcdf(os.path.join(config["out_path"], f"model_output_{date_str}.nc"))
+
