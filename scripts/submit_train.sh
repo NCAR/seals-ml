0e1864eb5d280ab075c837b83188209a6cb447b7
charlie-becker <cbecker@ucar.edu>
2023-12-05 14:43:14 -0700

    train updates

--- scripts/submit_train.sh
+++ scripts/submit_train.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -l
 #PBS -N seals
 #PBS -A NAML0001
-#PBS -l walltime=1:00:00
-#PBS -o seals_train.out
-#PBS -e seals_train.out
-#PBS -q casper
-#PBS -l select=1:ncpus=12:ngpus=1:mem=64GB -l gpu_type=v100
+#PBS -l walltime=4:00:00
+#PBS -o seals_train_der.out
+#PBS -e seals_train_der.out
+#PBS -q main
+#PBS -l select=1:ngpus=4:mem=128GB -l gpu_type=a100
 #PBS -m a
 #PBS -M cbecker@ucar.edu
+module load conda
 conda activate seals
 cd /glade/work/cbecker/seals-ml/
 python -u ./scripts/train.py  -c ./config/train_transformer.yaml
