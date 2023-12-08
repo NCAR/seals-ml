c3577bedf4376e885a9c5d1f936178405fcdba82
Thomas Martin <38228140+ThomasMGeo@users.noreply.github.com>
2023-12-07 17:39:55 -0700

    Updating API to be more scikit-learn like
    Updating to have a more scikit-learn type interface.

--- sealsml/baseline.py
+++ sealsml/baseline.py
@@ -10,6 +10,7 @@ from sklearn.ensemble import RandomForestRegressor
 
 ## Need a few functuons for the baseline ML, since the inputs are different enough did not put them in the class
 
+# This is not an interpolator
 def create_meshgrid(x_sensors, y_sensors, buffer=6, grid_points=100):
     """
     Create a meshgrid based on sensor positions.
@@ -27,127 +28,196 @@ def create_meshgrid(x_sensors, y_sensors, buffer=6, grid_points=100):
     x_new, y_new = np.mgrid[min(x_sensors)-buffer:max(x_sensors)+buffer:grid_points*1j, min(y_sensors)-buffer:max(y_sensors)+buffer:grid_points*1j]
     return x_new, y_new
 
-
-class BaselineModels(object):
-
-  def __init__(self, x_sensors, y_sensors, z_sensors, x_mesh, y_mesh):
-
-    self.x_sensors = x_sensors
-    self.y_sensors = y_sensors
-    self.z_sensors = z_sensors
-
-    self.x_mesh = x_mesh
-    self.y_mesh = y_mesh
-
-  def scipy_interpolate(self, method='cubic'):
+# Everything below here should be an interpolator
+class ScipyInterpolate(object):
     """
-    Perform 2D interpolation using scipy's griddata and find the global maximum and its indices.
+    A scikit-learn compatible class for performing 2D interpolation using scipy's griddata and finding the global maximum.
 
     Parameters:
-    - x_sensors (array-like): X-coordinates of the sensor data.
-    - y_sensors (array-like): Y-coordinates of the sensor data.
-    - z_sensors (array-like): Sensor values corresponding to (x_sensors, y_sensors).
-    - x_mesh (array-like): X-coordinates for which to interpolate.
-    - y_mesh (array-like): Y-coordinates for which to interpolate.
     - method (str, optional): Interpolation method, default is 'cubic'.
-
-    Returns:
-    - z_interpolated (ndarray): Interpolated values at the specified coordinates (x_new, y_new).
-    - max_z (float): Global maximum value of the interpolated data.
-    - max_indices (tuple): Indices of the global maximum in the interpolated data.
-
-    Note:
-    - The function uses scipy's griddata for 2D interpolation.
-    - The default interpolation method is cubic, but other methods can be specified.
     """
-    # Performing 2D interpolation
-    z_interpolated = griddata((self.x_sensors, self.y_sensors), self.z_sensors, (self.x_mesh, self.y_mesh), method=method)
 
-    # Finding Global Max and its indices
-    max_z = np.nanmax(z_interpolated)
-    max_indices = np.where(z_interpolated == max_z)
+    def __init__(self, method="cubic"):
+        self.method = method
 
-    return z_interpolated, max_z, max_indices
+    def fit(self, x, y, z):
+        """
+        Stores the sensor data for later use in the predict method.
+
+        Parameters:
+        - x (array-like): X-coordinates of the sensor data.
+        - y (array-like): Y-coordinates of the sensor data.
+        - z (array-like): Sensor values corresponding to (x, y).
+
+        Returns:
+        - self (ScipyInterpolate): Fitted instance.
+        """
+        self.x_sensors_ = x
+        self.y_sensors_ = y
+        self.z_sensors_ = z
+        return self
+
+    def predict(self, x_mesh, y_mesh):
+        """
+        Performs interpolation and finds the global maximum on the provided mesh coordinates using the stored sensor data.
+
+        Parameters:
+        - x_mesh (array-like): X-coordinates for which to interpolate.
+        - y_mesh (array-like): Y-coordinates for which to interpolate.
+
+        Returns:
+        - z_interpolated (ndarray): Interpolated values at the specified coordinates.
+        - max_z (float): Global maximum value of the interpolated data.
+        - max_indices (tuple): Indices of the global maximum in the interpolated data.
+
+        Note:
+        - The function uses the interpolation method specified during initialization.
+        """
+        from scipy.interpolate import griddata
+
+        z_interpolated = griddata((self.x_sensors_, self.y_sensors_), self.z_sensors_, (x_mesh, y_mesh), method=self.method)
+        max_z = np.nanmax(z_interpolated)
+        max_indices = np.where(z_interpolated == max_z)
+
+        return z_interpolated, max_z, max_indices
   
-  def gaussian_process_interpolation(self, length_scale=10):
+class GaussianProcessInterpolator():
     """
-    Perform Gaussian Process interpolation on the given data.
+    A scikit-learn compatible class for performing Gaussian Process interpolation.
 
     Parameters:
-    - X_train (array-like): Training data features.
-    - y_train (array-like): Training data labels.
-    - x_new (array-like): New data features for interpolation.
-    - y_new (array-like): New data labels for interpolation.
-    - length_scale (float, optional): Length scale for the RBF kernel. Default is 1.
-
-    Returns:
-    - reshaped_gp_results (numpy.ndarray): Interpolated values reshaped to the shape of x_new and y_new.
-    - max_z_gp (float): Global maximum value in the interpolated grid.
-    - max_indices_gp (tuple): Indices of the global maximum value in the interpolated grid.
+    - length_scale (float, optional): Length scale for the RBF kernel. Default is 10.
     """
 
-    X_train = np.column_stack((self.x_sensors, self.y_sensors))
-    y_train = self.z_sensors
+    def __init__(self, length_scale=10):
+        self.length_scale = length_scale
 
-    # Define the kernel for Gaussian Process (RBF kernel is used here)
-    kernel = RBF(length_scale=length_scale)
+    def fit(self, x, y, z):
+        """
+        Fits the Gaussian process model with the sensor data.
 
-    # Create the Gaussian Process Regressor
-    gp_model = GaussianProcessRegressor(kernel=kernel)
+        Parameters:
+        - x_sensors (array-like): X-coordinates of the sensor data.
+        - y_sensors (array-like): Y-coordinates of the sensor data.
+        - z_sensors (array-like): Sensor values corresponding to (x_sensors, y_sensors).
 
-    # Fit the model with the data
-    gp_model.fit(X_train, y_train)
+        Returns:
+        - self (GaussianProcessInterpolator): Fitted instance.
+        """
+        self.x_sensors_ = x
+        self.y_sensors_ = y
+        self.z_sensors_ = z
 
-    # Make some test data
-    X_test = np.column_stack((self.x_mesh.ravel(), self.y_mesh.ravel()))
+        # Combine sensor data into training features
+        x_train = np.column_stack((self.x_sensors_, self.y_sensors_))
 
-    # Predict interpolated values
-    interpolated_values_gp, std = gp_model.predict(X_test, return_std=True)
-    reshaped_gp_results = interpolated_values_gp.reshape(self.x_mesh.shape)
 
-    # Finding Global Max and its indices using the Gaussian process model
-    max_z_gp = np.nanmax(reshaped_gp_results)
-    max_indices_gp = np.where(max_z_gp == reshaped_gp_results)
 
-    return reshaped_gp_results, max_z_gp, max_indices_gp
+        # Fit the Gaussian process model
+        self.gp_model = GaussianProcessRegressor(kernel=RBF(length_scale=self.length_scale))
+        self.gp_model.fit(x_train, self.z_sensors_)
 
-  def random_forest_interpolation(self, max_depth=2, n_estimators=50, random_state=42):
+        return self
+
+    def predict(self, x_mesh, y_mesh):
+        """
+        Performs interpolation on the provided mesh coordinates using the fitted Gaussian Process model.
+
+        Parameters:
+        - x_mesh (array-like): X-coordinates for interpolation.
+        - y_mesh (array-like): Y-coordinates for interpolation.
+
+        Returns:
+        - interpolated_values (ndarray): Interpolated values at the specified coordinates.
+        - max_z (float): Global maximum value of the interpolated data.
+        - max_indices (tuple): Indices of the global maximum in the interpolated data.
+        """
+       
+       
+        # Combine mesh coordinates into test features
+        x_test = np.column_stack((x_mesh.ravel(), y_mesh.ravel()))
+        print('shape of x_test', np.shape(x_test))
+        # Predict interpolated values
+        interpolated_values = self.gp_model.predict(x_test)
+
+        # Reshape interpolated values to match the mesh dimensions
+        interpolated_values = interpolated_values.reshape(x_mesh.shape)
+
+        # Finding Global Max and its indices using the Gaussian process model
+        max_z = np.nanmax(interpolated_values)
+        max_indices = np.where(max_z == interpolated_values)
+
+        return interpolated_values, max_z, max_indices
+
+class RandomForestInterpolator():
     """
-    Perform Random Forest interpolation on the given data.
+    A scikit-learn compatible class for performing Random Forest interpolation.
 
     Parameters:
-    - X_train (array-like): Training data features.
-    - y_train (array-like): Training data labels.
-    - x_new (array-like): New data features for interpolation.
-    - y_new (array-like): New data labels for interpolation.
     - max_depth (int, optional): Maximum depth of the decision trees. Default is 2.
     - n_estimators (int, optional): Number of trees in the forest. Default is 50.
     - random_state (int, optional): Seed for random number generation. Default is 42.
-
-    Returns:
-    - reshaped_rf_results (numpy.ndarray): Interpolated values reshaped to the shape of x_new and y_new.
-    - max_z_rf (float): Global maximum value in the interpolated grid.
-    - max_indices_rf (tuple): Indices of the global maximum value in the interpolated grid.
     """
 
-    X_train = np.column_stack((self.x_sensors, self.y_sensors))
-    y_train = self.z_sensors
+    def __init__(self, max_depth=2, n_estimators=50, random_state=42):
+        self.max_depth = max_depth
+        self.n_estimators = n_estimators
+        self.random_state = random_state
 
-    # Create the Random Forest Regressor
-    rf_model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, random_state=random_state)
+    def fit(self, x, y, z):
+        """
+        Fits the Random Forest model with the sensor data.
 
-    # Fit the model with the data
-    rf_model.fit(X_train, y_train)
+        Parameters:
+        - x_sensors (array-like): X-coordinates of the sensor data.
+        - y_sensors (array-like): Y-coordinates of the sensor data.
+        - z_sensors (array-like): Sensor values corresponding to (x_sensors, y_sensors).
 
-    # Make some test data
-    X_test = np.column_stack((self.x_mesh.ravel(), self.y_mesh.ravel()))
+        Returns:
+        - self (RandomForestInterpolator): Fitted instance.
+        """
 
-    # Predict interpolated values
-    interpolated_values_rf = rf_model.predict(X_test)
-    reshaped_rf_results = interpolated_values_rf.reshape(self.x_mesh.shape)
+        self.x_sensors_ = x
+        self.y_sensors_ = y
+        self.z_sensors_ = z
 
-    # Finding Global Max and its indices using the Random Forest model
-    max_z_rf = np.nanmax(reshaped_rf_results)
-    max_indices_rf = np.unravel_index(np.argmax(reshaped_rf_results), reshaped_rf_results.shape)
 
-    return reshaped_rf_results, max_z_rf, max_indices_rf
+        # Combine sensor data into training features
+        x_train = np.column_stack((self.x_sensors_, self.y_sensors_))
+
+        # Create the Random Forest Regressor
+        self.rf_model = RandomForestRegressor(n_estimators=self.n_estimators, max_depth=self.max_depth, random_state=self.random_state)
+
+        # Fit the model with the data
+        self.rf_model.fit(x_train, self.z_sensors_)
+
+        return self
+
+    def predict(self, x_mesh, y_mesh):
+        """
+        Performs interpolation on the provided mesh coordinates using the fitted Random Forest model.
+
+        Parameters:
+        - x_mesh (array-like): X-coordinates for interpolation.
+        - y_mesh (array-like): Y-coordinates for interpolation.
+
+        Returns:
+        - interpolated_values (ndarray): Interpolated values at the specified coordinates.
+        - max_z (float): Global maximum value of the interpolated data.
+        - max_indices (tuple): Indices of the global maximum in the interpolated data.
+        """
+        # Combine mesh coordinates into test features
+        x_test = np.column_stack((x_mesh.ravel(), y_mesh.ravel()))
+
+        # Predict interpolated values
+        interpolated_values_rf = self.rf_model.predict(x_test)
+
+        # Reshape interpolated values to match the mesh dimensions
+        interpolated_values_rf = interpolated_values_rf.reshape(x_mesh.shape)
+
+        # Finding Global Max and its indices using the Random Forest model
+        max_z_rf = np.nanmax(interpolated_values_rf)
+        max_indices_rf = np.unravel_index(np.argmax(interpolated_values_rf), interpolated_values_rf.shape)
+
+        return interpolated_values_rf, max_z_rf, max_indices_rf
+    
