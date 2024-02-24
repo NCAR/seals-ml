e82ceae6a1e0d673877ff7803f20fefbfa7d0363
charlie-becker <cbecker@ucar.edu>
2024-02-23 23:09:13 -0700

    add tests for new models (and data)

--- tests/test_keras_models.py
+++ tests/test_keras_models.py
@@ -1,39 +1,77 @@
 from sealsml.keras.layers import VectorQuantizer
-from sealsml.keras.models import QuantizedTransformer
+from sealsml.keras.models import QuantizedTransformer, TEncoder, BackTrackerDNN
+from sealsml.data import Preprocessor
 import numpy as np
 
+test_data = ["../test_data/training_data_CBL2m_Ug10_src1-8kg_a.3.nc"]
+p = Preprocessor(scaler_type="quantile", sensor_pad_value=-1, sensor_type_value=-999)
+encoder_data, decoder_data, y, y_leak_rate = p.load_data(test_data)
+x_encoder, encoder_mask = p.preprocess(encoder_data, fit_scaler=True)
+x_decoder, decoder_mask = p.preprocess(decoder_data, fit_scaler=False)
+batch_size = x_encoder.shape[0]
 
 def test_quantized_transformer():
+
     from keras.models import load_model
-    batch_size = 256
-    train_size = 1024
-    encoder_sensor_size = 12
-    encoder_seq_size = 100
-    decoder_seq_size = 5
-    n_vars = 5
     np.random.seed(32525)
-    x_encoder = np.random.random((train_size, encoder_sensor_size, encoder_seq_size, n_vars))
-    x_decoder = np.random.random((train_size, decoder_seq_size, n_vars))
-    y = np.zeros((train_size, decoder_seq_size))
-    y[x_decoder[:, :, 1].argmax(axis=0)] = 1
+    print("x encoder shape", x_encoder.shape)
+    print("x decoder shape", x_decoder.shape)
     qt = QuantizedTransformer(use_quantizer=True, encoder_layers=2, decoder_layers=2)
     qt.compile(loss="binary_crossentropy", optimizer="adam")
-    # qt.call((x_encoder, x_decoder))
-    qt.fit((x_encoder, x_decoder), y, batch_size=batch_size, epochs=1)
+    qt.call((x_encoder, x_decoder))
     weights_init = qt.get_weights()
     qt.fit((x_encoder, x_decoder), y, batch_size=batch_size, epochs=1)
     weights_after = qt.get_weights()
     weights_constant = [np.all(s == e) for s, e in zip(weights_init, weights_after)]
     assert not np.any(weights_constant), "Weights are not changing somewhere"
-    y_pred = qt.predict([x_encoder, x_decoder], batch_size=train_size)
+    y_pred = qt.predict([x_encoder, x_decoder], batch_size=batch_size)
     assert y_pred[:, :, 0].shape == y.shape
     qt.save("./test_model.keras")
     new_qt = load_model("./test_model.keras", custom_objects={"VectorQuantizer": VectorQuantizer})
-    y_pred_new = new_qt.predict([x_encoder, x_decoder], batch_size=train_size)
+    y_pred_new = new_qt.predict([x_encoder, x_decoder], batch_size=batch_size)
     max_pred_diff = np.max(np.abs(y_pred - y_pred_new))
     assert max_pred_diff == 0, f"predictions change by max {max_pred_diff}"
-    print()
-    print(qt.summary())
-    print(new_qt.summary())
     assert new_qt.summary() == qt.summary(), "models do not match"
 
+def test_transformer_regressor():
+    from keras.models import load_model
+    np.random.seed(32525)
+    print("x encoder shape", x_encoder.shape)
+    print("x decoder shape", x_decoder.shape)
+    model = TEncoder(use_quantizer=True, encoder_layers=2)
+    model.compile(loss="mse", optimizer="adam")
+    model.call((x_encoder, x_decoder))
+    weights_init = model.get_weights()
+    model.fit((x_encoder, x_decoder), y_leak_rate, batch_size=batch_size, epochs=1)
+    weights_after = model.get_weights()
+    weights_constant = [np.all(s == e) for s, e in zip(weights_init, weights_after)]
+    assert not np.any(weights_constant), "Weights are not changing somewhere"
+    y_pred = model.predict([x_encoder, x_decoder], batch_size=batch_size)
+    assert y_pred[:, 0].shape == y_leak_rate.shape
+    model.save("./test_model_2.keras")
+    new_model = load_model("./test_model_2.keras", custom_objects={"VectorQuantizer": VectorQuantizer})
+    y_pred_new = new_model.predict([x_encoder, x_decoder], batch_size=batch_size)
+    max_pred_diff = np.max(np.abs(y_pred - y_pred_new))
+    assert max_pred_diff == 0, f"predictions change by max {max_pred_diff}"
+    assert new_model.summary() == model.summary(), "models do not match"
+
+
+def test_backtracker():
+    from keras.models import load_model
+    np.random.seed(32525)
+    print("x encoder shape", x_encoder.shape)
+    print("x decoder shape", x_decoder.shape)
+    model = BackTrackerDNN()
+    weights_init = model.get_weights()
+    model.fit((x_encoder, x_decoder), y)
+    weights_after = model.get_weights()
+    weights_constant = [np.all(s == e) for s, e in zip(weights_init, weights_after)]
+    assert not np.any(weights_constant), "Weights are not changing somewhere"
+    y_pred = model.predict([x_encoder, x_decoder], batch_size=batch_size)
+    assert y_pred[:, 0].shape == y.shape
+    model.save("./test_model_3.keras")
+    new_model = load_model("./test_model_3.keras")
+    y_pred_new = new_model.predict([x_encoder, x_decoder], batch_size=batch_size)
+    max_pred_diff = np.max(np.abs(y_pred - y_pred_new))
+    assert max_pred_diff == 0, f"predictions change by max {max_pred_diff}"
+    assert new_model.summary() == model.summary(), "models do not match"
