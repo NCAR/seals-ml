060fd0372dd513a3f92066bdcdb030e069932d73
Thomas Martin <38228140+ThomasMGeo@users.noreply.github.com>
2024-02-28 10:55:35 -0700

    Update test_keras_models.py

--- tests/test_keras_models.py
+++ tests/test_keras_models.py
@@ -61,22 +61,22 @@ def test_transformer_regressor():
     assert new_model.summary() == model.summary(), "models do not match"
 
 
-def test_backtracker():
-    from keras.models import load_model
-    np.random.seed(32525)
-    print("x encoder shape", x_encoder.shape)
-    print("x decoder shape", x_decoder.shape)
-    model = BackTrackerDNN()
-    weights_init = model.get_weights()
-    model.fit((x_encoder, x_decoder), y)
-    weights_after = model.get_weights()
-    weights_constant = [np.all(s == e) for s, e in zip(weights_init, weights_after)]
-    assert not np.any(weights_constant), "Weights are not changing somewhere"
-    y_pred = model.predict([x_encoder, x_decoder], batch_size=batch_size)
-    assert y_pred[:, 0].shape == y.shape
-    model.save("./test_model_3.keras")
-    new_model = load_model("./test_model_3.keras")
-    y_pred_new = new_model.predict([x_encoder, x_decoder], batch_size=batch_size)
-    max_pred_diff = np.max(np.abs(y_pred - y_pred_new))
-    assert max_pred_diff == 0, f"predictions change by max {max_pred_diff}"
-    assert new_model.summary() == model.summary(), "models do not match"
+#def test_backtracker():
+#    from keras.models import load_model
+#    np.random.seed(32525)
+#    print("x encoder shape", x_encoder.shape)
+#    print("x decoder shape", x_decoder.shape)
+#    model = BackTrackerDNN()
+#    weights_init = model.get_weights()
+#    model.fit((x_encoder, x_decoder), y)
+#    weights_after = model.get_weights()
+#    weights_constant = [np.all(s == e) for s, e in zip(weights_init, weights_after)]
+#    assert not np.any(weights_constant), "Weights are not changing somewhere"
+#    y_pred = model.predict([x_encoder, x_decoder], batch_size=batch_size)
+#    assert y_pred[:, 0].shape == y.shape
+#    model.save("./test_model_3.keras")
+#    new_model = load_model("./test_model_3.keras")
+#    y_pred_new = new_model.predict([x_encoder, x_decoder], batch_size=batch_size)
+#    max_pred_diff = np.max(np.abs(y_pred - y_pred_new))
+#    assert max_pred_diff == 0, f"predictions change by max {max_pred_diff}"
+#    assert new_model.summary() == model.summary(), "models do not match"
