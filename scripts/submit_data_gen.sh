ca4929375b0f1bf49addd6517d3a60a455332df3
charlie-becker <cbecker@ucar.edu>
2024-03-26 13:57:57 -0600

    update submit scripts

--- scripts/submit_data_gen.sh
+++ scripts/submit_data_gen.sh
@@ -2,13 +2,13 @@
 #PBS -N seals
 #PBS -A NAML0001
 #PBS -l walltime=03:00:00
-#PBS -o seals_data_gen_DC.out
-#PBS -e seals_data_gen_DC.out
+#PBS -o seals_data_gen.out
+#PBS -e seals_data_gen.out
 #PBS -q main
-#PBS -l select=1:ncpus=18:mem=128GB
+#PBS -l select=1:ncpus=120:mem=128GB
 #PBS -m a
 #PBS -M cbecker@ucar.edu
 module load conda
-conda activate sealsml
+conda activate seals
 cd /glade/work/cbecker/seals-ml/
 python -u ./scripts/generate_data.py  -c ./config/generate_training_data.yaml
