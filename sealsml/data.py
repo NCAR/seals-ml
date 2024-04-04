ea6d05093bf3df80ea1d8eb7ba10d777c4ba6916
Thomas Martin <38228140+ThomasMGeo@users.noreply.github.com>
2024-04-04 15:25:47 -0600

    Update data.py

--- sealsml/data.py
+++ sealsml/data.py
@@ -53,6 +53,7 @@ class DataSampler(object):
         self.z_res = self.data['zPos'][1, 0, 0].values - self.data['zPos'][0, 0, 0].values
         self.leak_rate = self.data['srcAuxScMassSpecValue']
         self.leak_loc = self.data['srcAuxScLocation'].values
+        self.source_num = self.data.srcDim.values
         
         # add zero arrays for new derived variables
         for var in self.coord_vars:
