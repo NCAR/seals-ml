6c80689d63a8beb60a216b1d759cc2bcdbda263e
Thomas Martin <38228140+ThomasMGeo@users.noreply.github.com>
2024-05-03 12:51:39 -0600

    Update staticinference.py

--- sealsml/staticinference.py
+++ sealsml/staticinference.py
@@ -1,11 +1,12 @@
 # Data manipulation and analysis
 import numpy as np
+from numpy.typing import NDArray
 import xarray as xr
 
 # seals geo stuff
 from sealsml.geometry import get_relative_azimuth
 
-def load_inference(dataset: xr.Dataset):
+def load_inference(dataset: xr.Dataset, timestep: int) -> NDArray:
     
     ds = xr.open_dataset(dataset)
     ##
@@ -21,21 +22,21 @@ def load_inference(dataset: xr.Dataset):
 
     for i in ds.CH4Sensors.values:
       # get_relative_azimuth(u, v, x_ref, y_ref, z_ref, x_target, y_target, z_target, time_series=True):
-      output = get_relative_azimuth(u_met, # u
-                         v_met, # v
-                         XYZ_met[0][0], #x_ref 
-                         XYZ_met[0][1], #y_ref
-                         XYZ_met[0][2], #z_ref
-                         XYZ_ch4[i][0], #x_target
-                         XYZ_ch4[i][1], #y_target
-                         XYZ_ch4[i][2], #z_target
-                         time_series=True
-                          
+      output = get_relative_azimuth(
+                        u_met, # u
+                        v_met, # v
+                        XYZ_met[0][0], #x_ref 
+                        XYZ_met[0][1], #y_ref
+                        XYZ_met[0][2], #z_ref
+                        XYZ_ch4[i][0], #x_target
+                        XYZ_ch4[i][1], #y_target
+                        XYZ_ch4[i][2], #z_target
+                        time_series=True  
                            )
 
       ch4_data = ds['q_CH4'].values[i]
       encoder_array = np.vstack((output, w_met, ch4_data))
       encoder_array = np.expand_dims(encoder_array, axis=-1)
       encoder_arrays.append(encoder_array)
-
-    return np.concatenate(encoder_arrays, axis=-1)
+      returned_array = np.concatenate(encoder_arrays, axis=-1)
+    return returned_array
