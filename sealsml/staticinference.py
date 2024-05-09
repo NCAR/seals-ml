5a6b09b656b75e6011252015a6e8fbc2d8758951
Thomas Martin <38228140+ThomasMGeo@users.noreply.github.com>
2024-05-09 13:01:07 -0600

    Update staticinference.py

--- sealsml/staticinference.py
+++ sealsml/staticinference.py
@@ -77,10 +77,11 @@ def load_inference(dataset, sitemap, timestep: int) -> NDArray:
     leak_z = sitemap.zPos.values.ravel()[mask.values.ravel()]
     
     print('Number of possible leaks:', len(leak_z))
-    for a in range(len(leak_z )):
-      targets = get_relative_azimuth(
-                        u_met, # u
-                        v_met, # v
+    for b in range(num_complete_series):
+      for a in range(len(leak_z )):
+        targets = get_relative_azimuth(
+                        u_met[b*timestep:(b+1)*timestep], # u
+                        v_met[b*timestep:(b+1)*timestep], # v
                         XYZ_met[0][0], #x_ref 
                         XYZ_met[0][1], #y_ref
                         XYZ_met[0][2], #z_ref
@@ -89,8 +90,9 @@ def load_inference(dataset, sitemap, timestep: int) -> NDArray:
                         leak_z[a], #z_target
                         time_series=False  
                            )
-      target_list.append(targets)
+        target_list.append(targets)
     
     # Target array is currently number of targets, variables, time)
-    target_arrays = np.array(target_list, dtype=float)
+    print('Number of samples:', num_complete_series )    
+    target_arrays = np.array(target_list, dtype=float).reshape(num_complete_series, len(leak_z), 6, 1)
     return reshaped_array, target_arrays
