84c26cf0a46de357b2de6972914a282b634fb139
Bryan Travis <btravis@psi.edu>
2024-05-28 22:16:52 -0600

    reverting back to sigmoid activation function as default

--- sealsml/keras/models.py
+++ sealsml/keras/models.py
@@ -304,7 +304,7 @@ class BackTrackerDNN(keras.models.Model):
         evidential_coef: Evidential regularization coefficient.
         metrics: Optional list of metrics to monitor during training.
     """
-    def __init__(self, hidden_layers=3, hidden_neurons=64, activation="relu", output_activation="linear", optimizer="SGD", loss_weights=None,
+    def __init__(self, hidden_layers=3, hidden_neurons=64, activation="relu", output_activation="sigmoid", optimizer="SGD", loss_weights=None,
                  use_noise=False, noise_sd=0.01, lr=0.00001, use_dropout=False, dropout_alpha=0.1, batch_size=1,
                  epochs=10, kernel_reg=None, l1_weight=0.01, l2_weight=0.01, n_output_tasks=4, verbose=1, **kwargs):
 
