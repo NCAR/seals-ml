NCAR/seals-ml

commit e793279d4ad37d2a0f720f5d54fddc84a73543ed

Removed uneccesary code per feedback on PR #92.

sealsml/data.py
@@ -316,10 +316,7 @@ class DataSampler(object):
              np.random.shuffle(structmask_indices) #only randomizes the first dimension (rows)
         ### Set the true leak
         # Find the true leak indices
-        if False:
-            true_leak_i, true_leak_j, true_leak_k = self.findTrueLeakIndices()
-        else:
-            true_leak_i, true_leak_j, true_leak_k = self.findIndices(self.leak_loc[0], self.leak_loc[1], self.leak_loc[2])
+        true_leak_i, true_leak_j, true_leak_k = self.findIndices(self.leak_loc[0], self.leak_loc[1], self.leak_loc[2])
         # Look for the true leak in the potential leaks
         indx=np.squeeze(np.argwhere(np.all((structmask_indices-[true_leak_i,true_leak_j,true_leak_k]==0), axis=-1)))
         if indx.size > 0:  #the true leak location is already in the randomly ordered potential leaks
@@ -354,18 +351,6 @@ class DataSampler(object):
         k_indx = np.abs(self.z - zloc).argmin()
 
         return i_indx, j_indx, k_indx
-
-    def findTrueLeakIndices(self):
-        # x location for true leak loc
-        true_i = np.abs(self.x - self.leak_loc[0]).argmin()
-
-        # y location for true leak loc
-        true_j = np.abs(self.y - self.leak_loc[1]).argmin()
-
-        # z location for true leak loc
-        true_k = np.abs(self.z - self.leak_loc[2]).argmin()
-
-        return true_i, true_j, true_k
 
     def pad_along_axis(self, array, target_length, pad_value=0, axis=0):
         """ Pad numpy array along a single dimension. """

commit f7c2f860ccdbfa91d5bd41df06193d9ddf31eb7e

Minor bug-fix for issue introduced by commit e793279.

sealsml/data.py
@@ -266,7 +266,7 @@ class DataSampler(object):
         # Randomize the index of the true leak within the set of randomly located potential leaks
         true_leak_pos = np.random.choice(n_leaks, size=1)[0]
         # Find the true leak indices
-        true_leak_i, true_leak_j, true_leak_k = self.findTrueLeakIndices() 
+        true_leak_i, true_leak_j, true_leak_k = self.findIndices(self.leak_loc[0], self.leak_loc[1], self.leak_loc[2])
         # set the random-indexed potential leak to the true leak position
         i_leak[true_leak_pos] = true_leak_i  
         j_leak[true_leak_pos] = true_leak_j