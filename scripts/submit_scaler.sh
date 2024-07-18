7604d55850d294c7c244d2a6c0efbdf0d494e95e
djgagne@ou.edu <davidjohngagne@gmail.com>
2024-07-18 15:08:45 -0600

    Added num procs to script

--- scripts/submit_scaler.sh
+++ scripts/submit_scaler.sh
@@ -5,9 +5,9 @@
 #PBS -o seals_train_der.out
 #PBS -e seals_train_der.out
 #PBS -q casper
-#PBS -l select=1:ncpus=36:ngpus=0:mem=300GB
+#PBS -l select=1:ncpus=36:ngpus=0:mem=200GB
 #PBS -m a
 #PBS -M dgagne@ucar.edu
 module load conda
 conda activate seals_20240515
-python -u distributed_scaling.py  -c ../config/train_transformer.yaml
+python -u distributed_scaling.py  -c ../config/train_transformer.yaml -p 36
