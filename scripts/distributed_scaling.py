Fixed distributed scaling

scripts/distributed_scaling.py
@@ -42,7 +42,7 @@ def main():
     all_scalers_arr = np.array(all_scalers)
     total_scalers_arr = np.sum(all_scalers_arr, axis=0)
     save_scaler(total_scalers_arr[0], join(out_path, "coord_scaler.json"))
-    save_scaler(total_scalers_arr[0], join(out_path, "sensor_scaler.json"))
+    save_scaler(total_scalers_arr[1], join(out_path, "sensor_scaler.json"))
     return
 
 

sealsml/data.py
@@ -4,7 +4,7 @@ import numpy as np
 from os.path import join, exists
 from os import makedirs
 from scipy.ndimage import minimum_filter
-from sealsml.geometry import GeoCalculator, get_relative_azimuth, generate_sensor_positions_min_distance
+from sealsml.geometry import get_relative_azimuth, generate_sensor_positions_min_distance
 from bridgescaler import DeepQuantileTransformer, DeepMinMaxScaler, DeepStandardScaler
 from bridgescaler import DQuantileScaler, DMinMaxScaler, DStandardScaler, load_scaler, save_scaler
 