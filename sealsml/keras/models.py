cbc66c0c6f20389c5f0f8430f90ffaf524c0cf41
charlie-becker <cbecker@ucar.edu>
2024-08-12 16:42:22 -0600

    Fix sigmoid bug in BlockTrans

--- sealsml/keras/models.py
+++ sealsml/keras/models.py
@@ -119,8 +119,8 @@ class BlockTransformer(keras.models.Model):
                                                           decoder_padding_mask=decoder_padding_mask,
                                                           training=training)
         output = self.output_hidden(decoder_output)
+        output = ops.squeeze(output, axis=-1)
         if self.output_activation == "softmax":
-            output = ops.squeeze(output, axis=-1)
             output = self.output_activation_layer(output, mask=decoder_padding_mask, axis=-1)
             # output = ops.expand_dims(output, -1)
         else:
@@ -470,7 +470,8 @@ class TEncoder(keras.models.Model):
                                 "dropout_rate", "use_quantizer", "quantized_beta", "n_outputs", "min_filters",
                                 "kernel_size", "filter_growth_rate", "n_conv_layers", "pooling", "pool_size",
                                 "padding"]
-        self.kernel_reg = keras.regularizers.l2(0.001)
+        # self.kernel_reg = keras.regularizers.l2(0.001)
+        self.kernel_reg = None
 
         self.conv_encoder = ConvSensorEncoder(min_filters=self.min_filters, kernel_size=self.kernel_size,
                                               filter_growth_rate=self.filter_growth_rate,
@@ -523,11 +524,113 @@ class TEncoder(keras.models.Model):
                 encoder_output = self.vector_quantizers[f"vector_quantizer_{e:02d}"](encoder_output)
             encoder_output = self.encoder_transformers[e](encoder_output,
                                                           padding_mask=encoder_padding_mask)
-        print("ENCODER OUTPUT SHAPE:", encoder_output.shape)
+        encoder_output_flat = ops.reshape(encoder_output,
+                                          newshape=(-1, encoder_output.shape[-2] * encoder_output.shape[-1]))
+        output = self.output_hidden(encoder_output_flat)
+
+        return output
+
+    def get_config(self):
+        base_config = super().get_config()
+        parameter_config = {hp: getattr(self, hp) for hp in self.hyperparameters}
+        return {**base_config, **parameter_config}
+
+
+class BlockEncoder(keras.models.Model):
+    """
+    Transformer model with an optional vector quantizer layer in the encoder branch to produce sharper forecasts.
+
+    Parameters:
+        encoder_layers (int): Number of passes through the encoder transformer layer in the encoder branch.
+        decoder_layers (int): Number of passes through the decoder transformer layer in the decoder branch.
+        hidden_size (int): Size of the latent vector for each element of the sequence (token).
+        n_heads (int): number of heads for each attention layer.
+        num_quantized_embeddings (int): Number of vectors to store in the vector codebook. The vector codebook will have
+            a size of (num_quantized_embeddings, hidden_size).
+        hidden_activation (str): Choice of activation function for the hidden layers. Default relu.
+        output_activation (str): Choice of activation function for the output layer. Default sigmoid.
+        dropout_rate (float): Percentage of neurons to randomly set to 0 during train time.
+        use_quantizer (bool): Whether or not to use the VectorQuantizer layer in the model.
+        quantized_beta (float): Regularizer term for the commitment loss in the VectorQuantizer layer. Should be between
+            0.25 and 2.
+        n_outputs (int): Number of outputs being predicted.
+
+    """
+
+    def __init__(self, encoder_layers=1,
+                 hidden_size=128,
+                 n_heads=8,
+                 hidden_activation="relu",
+                 output_activation="sigmoid",
+                 dropout_rate=0.1,
+                 n_outputs=1,
+                 block_size=5,
+                 n_coords=4,
+                 **kwargs):
+        super().__init__(**kwargs)
+        assert encoder_layers > 0, "Should be at least 1 encoder layer"
+        self.encoder_layers = encoder_layers
+        assert hidden_size > 0, "hidden_size should be positive"
+        self.hidden_size = hidden_size
+        assert n_heads > 0, "n_heads should be positive"
+        self.n_heads = n_heads
+        self.hidden_activation = hidden_activation
+        assert 0 <= dropout_rate < 1, "dropout rate should be between 0 and 1"
+        self.dropout_rate = dropout_rate
+        self.output_activation = output_activation
+        self.n_outputs = n_outputs
+        self.block_size = block_size
+        self.n_coords = n_coords
+        self.hyperparameters = ["encoder_layers", "hidden_size", "n_heads",
+                                "hidden_activation", "output_activation",
+                                "dropout_rate", "n_outputs", "block_size", "n_coords"]
+        self.kernel_reg = None
+        self.time_block_sensor_encoder = TimeBlockSensorEncoder(embedding_size=self.hidden_size,
+                                                                block_size=self.block_size,
+                                                                n_coords=self.n_coords)
+        self.encoder_hidden = layers.Dense(self.hidden_size, activation=self.hidden_activation,
+                                           kernel_regularizer=self.kernel_reg, name="encoder_hidden")
+        self.encoder_transformers = []
+        for n in range(self.encoder_layers):
+            self.encoder_transformers.append(TransformerEncoder(intermediate_dim=self.hidden_size,
+                                                                num_heads=self.n_heads,
+                                                                dropout=self.dropout_rate,
+                                                                activation=self.hidden_activation,
+                                                                name=f"encoder_transformer_{n:02d}"))
+
+        self.output_hidden = layers.Dense(self.n_outputs, activation=self.output_activation, name="output_hidden")
+
+        return
+
+    def call(self, inputs, training=False):
+        """
+        Args:
+            inputs (tuple): Inputs should contain at most
+                (encoder_input, decoder_input, encoder_padding_mask, decoder_padding_mask) but the mask variables
+                are optional. Using this order is required.
+            training (bool): if True run the layers in training mode.
+
+        """
+        # First inputs element is the encoder input, which would be the sensors.
+        encoder_input = inputs[0]
+        print("Encoder input shape:", encoder_input.shape)
+        # Second inputs element is the decoder input, which would be the potential leak locations.
+        encoder_padding_mask = None
+        encoder_shape = ops.shape(encoder_input)
+        if len(inputs) > 2:
+            # Repeat the encoder padding mask values for each time block.
+            # Output shape should be (batch_size, n_sensors * n_times / block_size )
+            encoder_padding_mask = ops.repeat(inputs[2], int(encoder_shape[2] // self.block_size), axis=1)
+        encoder_conv_out = self.time_block_sensor_encoder(encoder_input)
+        encoder_hidden_out = self.encoder_hidden(encoder_conv_out)
+        encoder_output = self.encoder_transformers[0](encoder_hidden_out,
+                                                      padding_mask=encoder_padding_mask)
+        for e in range(1, self.encoder_layers):
+            encoder_output = self.encoder_transformers[e](encoder_output,
+                                                          padding_mask=encoder_padding_mask)
 
         encoder_output_flat = ops.reshape(encoder_output,
                                           newshape=(-1, encoder_output.shape[-2] * encoder_output.shape[-1]))
-        print("ENCODER FLATTENED OUTPUT SHAPE:", encoder_output_flat.shape)
         output = self.output_hidden(encoder_output_flat)
 
         return output
