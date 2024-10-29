e2b507cb202ca7c1cb1f8d81ea45ca47a8c0051b
David John Gagne <davidjohngagne@gmail.com>
2024-10-28 18:10:23 -0600

    Deleted tests for removed models and layers.

--- tests/test_keras_layers.py
+++ tests/test_keras_layers.py
@@ -1,23 +1,11 @@
-from sealsml.keras.layers import ConvSensorEncoder
-from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from sealsml.keras.layers import TimeBlockSensorEncoder
+
 import numpy as np
 
-def test_convsensorencoder():
-    min_filters = 8
+def test_time_block_sensor_encoder():
     x_shape = (128, 5, 100, 3)
     x_test = np.random.random(x_shape)
     y = x_test.reshape(x_shape[0], x_shape[1] * x_shape[2] * x_shape[3]).mean(axis=1)
-    conv_encoder = ConvSensorEncoder(min_filters=min_filters)
-    conv_config = conv_encoder.get_config()
-    x_encoded = conv_encoder(x_test)
-    assert len(x_encoded.shape) == 3
-    assert x_encoded.shape[0] == x_shape[0] and x_encoded.shape[1] == x_shape[1]
-    assert conv_config["min_filters"] == min_filters
-    model = Sequential(layers=[ConvSensorEncoder(min_filters=min_filters, input_shape=x_shape[1:]),
-                               Flatten(), Dense(1)])
-    model.compile(loss="mse", optimizer="adam")
-    model.fit(x_test, y, epochs=10)
-    y_pred = model.predict(x_test)
-    assert y_pred.shape == (x_shape[0], 1)
+    tbse = TimeBlockSensorEncoder(embedding_size=128, block_size=10)
+    tbse_out = tbse(x_test)
     return
--- tests/test_keras_models.py
+++ tests/test_keras_models.py
@@ -1,5 +1,4 @@
-from sealsml.keras.layers import VectorQuantizer
-from sealsml.keras.models import QuantizedTransformer, TEncoder, BackTrackerDNN, BlockTransformer, LocalizedLeakRateBlockTransformer, BlockEncoder
+from sealsml.keras.models import BackTrackerDNN, BlockTransformer, LocalizedLeakRateBlockTransformer, BlockEncoder
 from sealsml.data import Preprocessor
 from sealsml.backtrack import backtrack_preprocess, create_binary_preds_relative, truth_values
 from sealsml.keras.metrics import mean_searched_locations
@@ -106,28 +105,6 @@ def test_localized_leak_rate_block_transformer():
     max_pred_diff = np.max(np.abs(y_pred[0] - y_pred_new[0]))
     assert max_pred_diff == 0, f"predictions change by max {max_pred_diff}"
 
-
-def test_quantized_transformer():
-
-    np.random.seed(32525)
-    print("x encoder shape", x_encoder.shape)
-    print("x decoder shape", x_decoder.shape)
-    qt = QuantizedTransformer(use_quantizer=True, encoder_layers=2, decoder_layers=2)
-    qt.compile(loss="binary_crossentropy", optimizer="adam")
-    qt.call((x_encoder, x_decoder))
-    weights_init = qt.get_weights()
-    qt.fit((x_encoder, x_decoder), y, batch_size=batch_size, epochs=2)
-    weights_after = qt.get_weights()
-    weights_constant = [np.all(s == e) for s, e in zip(weights_init, weights_after)]
-    assert not np.any(weights_constant), "Weights are not changing somewhere"
-    y_pred = qt.predict([x_encoder, x_decoder], batch_size=batch_size)
-    assert y_pred[:, :, 0].shape == y.shape
-    qt.save("./test_model.keras")
-    new_qt = load_model("./test_model.keras", custom_objects={"VectorQuantizer": VectorQuantizer})
-    y_pred_new = new_qt.predict([x_encoder, x_decoder], batch_size=batch_size)
-    max_pred_diff = np.max(np.abs(y_pred - y_pred_new))
-    assert max_pred_diff == 0, f"predictions change by max {max_pred_diff}"
-    # assert new_qt.summary() == qt.summary(), "models do not match"
 
 def test_transformer_regressor():
     np.random.seed(32525)
